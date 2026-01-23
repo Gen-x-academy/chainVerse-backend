c7ad7bd27453f63528b743b5dbecc5b1106dc104
ekene <nwobodoe71@gmail.com>
2026-01-23 09:01:19 +0100

    feat(recommendation): implement recommendation service logic

---
+++ src/modules/recommendation.service.js
@@ -0,0 +1,32 @@
+const Course = require('../../models/course');
+const Enrollment = require('../../models/enrollment');
+const ChallengeResult = require('../../models/ChallengeResult');
+const { rules } = require('./recommendation.rules');
+
+exports.getNextCourseRecommendations = async (userId) => {
+  const enrollments = await Enrollment.find({ student: userId, completed: true })
+    .populate('course');
+
+  if (!enrollments.length) {
+    return [];
+  }
+
+  const completedCourses = enrollments.map(e => e.course);
+  const allCourses = await Course.find({ isPublished: true });
+
+  const quizResults = await ChallengeResult.find({ playerOneId: userId });
+
+  const context = {
+    completedCourses,
+    allCourses,
+    quizResults,
+    remedialCourses: allCourses.filter(c => c.isRemedial),
+  };
+
+  const recommendations = rules.flatMap(rule => rule.apply(context));
+
+  const unique = new Map();
+  recommendations.forEach(r => unique.set(r.courseId, r));
+
+  return Array.from(unique.values());
+};
