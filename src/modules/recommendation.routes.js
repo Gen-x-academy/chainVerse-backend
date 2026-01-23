ab056627187951a3e96893b3e79b5700a3598d79
ekene <nwobodoe71@gmail.com>
2026-01-23 09:31:04 +0100

    docs(recommendation): document recommendation endpoint in swagger

--- src/modules/recommendation.routes.js
+++ src/modules/recommendation.routes.js
@@ -1,9 +1,24 @@
-const router = require('express').Router();
-const { authMiddleware } = require('../../middlewares/authMiddleware');
-const { getNextCourses } = require('./recommendation.controller');
+const router = require("express").Router();
+const { authMiddleware } = require("../../middlewares/authMiddleware");
+const { getNextCourses } = require("./recommendation.controller");
 
-
-
-router.get('/next-courses', authMiddleware, getNextCourses);
+/**
+ * @swagger
+ * /api/recommendation/next-courses:
+ *   get:
+ *     summary: Get recommended next courses
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Recommended courses
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Server error
+ */
+router.get("/next-courses", authMiddleware, getNextCourses);
 
 module.exports = router;
