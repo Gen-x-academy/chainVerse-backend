4ab8747221da32923daa08ccbe41da5c1d9956c6
authenticeasy-sys <authenticeasy@gmail.com>
2026-02-21 15:42:15 +0100

    Implemented course feedback

---
+++ src/express/course-ratings-feedback/service.js
@@ -0,0 +1,5 @@
+const { createIssueService } = require('../../../common/create-issue-module');
+
+const issue_10_course_ratings_feedbackService = createIssueService('course-ratings-feedback');
+
+module.exports = { issue_10_course_ratings_feedbackService };
