1b381a9db24b87f51f597617ce2e52b15e853cde
Gbolahan Akande <geakande@gmail.com>
2026-02-25 14:36:43 +0100

    Register all new modules in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,13 @@ import { OrganizationModule } from './organization/organization.module';
 import { OrganizationMemberModule } from './organization-member/organization-member.module';
 import { PointsModule } from './points/points.module';
 import { RemovalRequestModule } from './removal-request/removal-request.module';
+import { ReportAbuseModule } from './report-abuse/report-abuse.module';
+import { SessionModule } from './session/session.module';
+import { SubscriptionPlanModule } from './subscription-plan/subscription-plan.module';
+import { CourseRatingsFeedbackModule } from './course-ratings-feedback/course-ratings-feedback.module';
+import { StudentSavedCoursesModule } from './student-saved-courses/student-saved-courses.module';
+import { GoogleAuthModule } from './google-auth/google-auth.module';
+import { StudentCartModule } from './student-cart/student-cart.module';
 
 @Module({
   imports: [
@@ -18,6 +25,13 @@ import { RemovalRequestModule } from './removal-request/removal-request.module';
     OrganizationMemberModule,
     PointsModule,
     RemovalRequestModule,
+    ReportAbuseModule,
+    SessionModule,
+    SubscriptionPlanModule,
+    CourseRatingsFeedbackModule,
+    StudentSavedCoursesModule,
+    GoogleAuthModule,
+    StudentCartModule,
   ],
   controllers: [AppController],
   providers: [AppService],
