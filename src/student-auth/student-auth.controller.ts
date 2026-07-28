e1265697e3b3ff44f904dacf88246b9dd2bb3e54
nafiuishaaq <nafiuig@gmail.com>
2026-07-28 10:23:10 +0100

    implemneted the session

--- src/student-auth/student-auth.controller.ts
+++ src/student-auth/student-auth.controller.ts
@@ -94,8 +94,8 @@ export class StudentAuthController {
     status: 401,
     description: 'Invalid credentials or unverified email',
   })
-  login(@Body() dto: LoginStudentDto) {
-    return this.studentAuthService.login(dto);
+  login(@Body() dto: LoginStudentDto, @Req() req: Request) {
+    return this.studentAuthService.login(dto, (req as any).ip, req.headers['user-agent']);
   }
 
   @Throttle({ default: { limit: 3, ttl: 900_000 } }) // 3 per 15 minutes
