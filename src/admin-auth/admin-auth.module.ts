6d2d83c579eac0f40e9822afb7e4ddf5e79c540f
ummarig <nafiuigee@gmail.com>
2026-07-28 11:06:33 +0100

    implemented the auth

--- src/admin-auth/admin-auth.module.ts
+++ src/admin-auth/admin-auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
 import { AdminAuthController } from './admin-auth.controller';
 import { AdminAuthService } from './admin-auth.service';
+import { Admin, AdminSchema } from './schemas/admin.schema';
+import {
+  AdminRefreshToken,
+  AdminRefreshTokenSchema,
+} from './schemas/admin-refresh-token.schema';
+import { AuditModule } from '../common/audit/audit.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Admin.name, schema: AdminSchema },
+      { name: AdminRefreshToken.name, schema: AdminRefreshTokenSchema },
+    ]),
+    JwtModule.register({}),
+    AuditModule,
+  ],
   controllers: [AdminAuthController],
   providers: [AdminAuthService],
+  exports: [AdminAuthService],
 })
 export class AdminAuthModule {}
